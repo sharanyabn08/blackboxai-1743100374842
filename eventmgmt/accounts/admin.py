d60b2b19b34eac0b351e05acaccbe7dd06afe566
sharanyabn08 <162794567+sharanyabn08@users.noreply.github.com>
2025-03-28 00:04:22 +0530

    Add eventmgmt/accounts/admin.py 1743100461074

---
+++ eventmgmt/accounts/admin.py
@@ -0,0 +1,23 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
+
+class CustomUserAdmin(UserAdmin):
+    list_display = ('username', 'email', 'is_organizer', 'is_staff')
+    list_filter = ('is_organizer', 'is_staff', 'is_superuser')
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        ('Personal info', {'fields': ('first_name', 'last_name', 'email')}),
+        ('Permissions', {
+            'fields': ('is_active', 'is_staff', 'is_superuser', 'is_organizer', 'groups', 'user_permissions'),
+        }),
+        ('Important dates', {'fields': ('last_login', 'date_joined')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'password1', 'password2', 'is_organizer'),
+        }),
+    )
+
+admin.site.register(CustomUser, CustomUserAdmin)
